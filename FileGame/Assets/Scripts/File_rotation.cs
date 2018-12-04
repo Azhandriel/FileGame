0131037a036012ef51cb086016e25e27217029f3
OmarAslam229 <omar.aslam14@gmail.com>
2018-12-04 15:13:33 +0200

    Ist Level Ready

---
+++ FileGame/Assets/Scripts/File_rotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class File_rotation : MonoBehaviour {
+
+   float speed;
+
+	// Use this for initialization
+	void Start () {
+
+        transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+
+        speed = Random.Range(80, 180);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
---
+++ FileGame/Assets/Scripts/GUI_HUD.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GUI_HUD : MonoBehaviour {
+
+    public static GUI_HUD instance;
+
+    public Text TextFile_Total_Text, ImageFile_Total_Text, VideoFile_Total_Text;
+    public Text TextFile_Collected_Text, ImageFile_Collected_Text, VideoFile_Collected_Text;
+
+    public Text gameOverReason;
+
+    public GameObject GamePlay_Mode,gameComplete_Mode, GameOver_Mode;
+
+    int TextFile_ToCollect, ImageFile_ToCollect, VideoFile_ToCollect;
+    int TextFile_Collected, ImageFile_Collected, VideoFile_Collected;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        TextFile_ToCollect = PlayerPrefs.GetInt("text_files_Collect", 1);
+        ImageFile_ToCollect = PlayerPrefs.GetInt("image_files_Collect", 1);
+        VideoFile_ToCollect = PlayerPrefs.GetInt("video_files_Collect", 1);
+
+        TextFile_Total_Text.text ="/"+ TextFile_ToCollect;
+        ImageFile_Total_Text.text = "/" + ImageFile_ToCollect;
+        VideoFile_Total_Text.text = "/" + VideoFile_ToCollect;
+
+        TextFile_Collected_Text.text = ImageFile_Collected_Text.text = VideoFile_Collected_Text.text = "0";
+        TextFile_Collected = ImageFile_Collected = VideoFile_Collected = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void fileCollected(string name)
+    {
+
+        if (name.Contains("TEXT"))
+        {
+            TextFile_Collected++;
+
+            if (TextFile_Collected <= TextFile_ToCollect)
+            {
+                TextFile_Collected_Text.text = "" + TextFile_Collected;
+            }
+            else
+            {
+               
+                gameOver("TEXT");
+            }
+        }
+
+        if (name.Contains("IMAGE"))
+        {
+            ImageFile_Collected++;
+
+            if (ImageFile_Collected <= ImageFile_ToCollect)
+            {
+                ImageFile_Collected_Text.text = "" + ImageFile_Collected;
+            }
+            else
+            {
+                
+                gameOver("IMAGE");
+            }
+        }
+
+        if (name.Contains("VIDEO"))
+        {
+            VideoFile_Collected++;
+
+            if (VideoFile_Collected <= VideoFile_ToCollect)
+            {
+                VideoFile_Collected_Text.text = "" + VideoFile_Collected;
+            }
+            else
+            {
+                gameOver("VIDEO");
+            }
+        }
+
+        if (TextFile_Collected == TextFile_ToCollect && ImageFile_Collected == ImageFile_ToCollect && VideoFile_Collected == VideoFile_ToCollect)
+        {
+            level_Complete();
+        }
+
+    }
+
+
+    void level_Complete()
+    {
+        PlayerLook.instance.stop_Movement();
+        GamePlay_Mode.SetActive(false);
+        gameComplete_Mode.SetActive(true);
+    }
+
+    void gameOver(string reason)
+    {
+        PlayerLook.instance.stop_Movement();
+        gameOverReason.text = reason;
+        GamePlay_Mode.SetActive(false);
+        GameOver_Mode.SetActive(true);
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+
+    public void Next()
+    {
+    }
+
+
+}
