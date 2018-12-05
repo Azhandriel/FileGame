0922a10f481b3d090bf678c78f0fcb42459a7f9d
schea264 <schea264@tlu.ee>
2018-12-05 14:26:16 +0200

    Add Tablet Prefabs

---
+++ FileGame/Assets/Scripts/tabletRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tabletRotate : MonoBehaviour {
+
+	// Use this for initialization
+
+	public float speed = 10f;
+
+	void Update () {
+
+		transform.Rotate(Vector3.right, speed * Time.deltaTime);
+
+	}
+}
